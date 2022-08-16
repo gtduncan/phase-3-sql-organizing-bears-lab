042f7a94e7be0aeb3e8bc7c525421e313d23b8cb
George <gtduncan@usc.edu>
2022-08-16 14:36:30 -0400

    commit tree 3d671fd78bb9b7590023ea5d6f695052e814530b

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    sex VARCHAR(255),
+    color VARCHAR(255),
+    temperament VARCHAR(255),
+    alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'Black', 'Nice', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'Brown', 'Mean', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabita', 7, 'F', 'White', 'Ugly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', 'Black', 'Sassy', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 1, 'F', 'Brown', 'Powerful', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'White', 'Cool', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'Black', 'Suave', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 9, 'M', 'Brown', 'Mean', false);
